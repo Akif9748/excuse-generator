48c14358133f2a0b69135eac68df3584c5b80a17
Akif Yüce <70021050+Akif9748@users.noreply.github.com>
2022-02-12 03:56:31 +0300

    Update to real JS syntax

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-excuses = [
+const excuses = [
     [ "I was ", "My neighbor was ", "We were ", "My mom was ", "My sister was ", "My brother was ", "My girlfriend was ", "My boyfriend was ", "My uncle was " ],
     [ "washing my ", "eating a ", "damaging a ", "praying to a ", "riding a ", "making a ", "going through a portal with a ", "invading America for a ", "forgot everything because of a " ],
     [ "car.", "cat.", "couch.", "door.", "television.", "computer case.", "carpet", "grenade.", "soldier.", "lettuce." ]
-];
+],
+generatedExcuse = [];
 
 // A funny list, you can create a pull request to add something :]
 
-generated_excuse = [];
+for (choiceFirst of excuses) 
+    generatedExcuse.push(choiceFirst[Math.floor(Math.random() * choiceFirst.length)]);
 
-for (choice_first of excuses) generated_excuse.push(choice_first[Math.floor(Math.random() * choice_first.length)])
-
-console.log(generated_excuse.join(""));
+console.log(generatedExcuse.join(""));
