efd3b5d87c3e9a040219fa359b87c7fbf31f7717
Akif9748 <akif9748@gmail.com>
2023-07-31 17:29:30 +0300

    Real JS

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const excuses = [
-    [ "I was ", "My neighbor was ", "We were ", "My mom was ", "My sister was ", "My brother was ", "My girlfriend was ", "My boyfriend was ", "My uncle was " ],
-    [ "washing my ", "eating a ", "damaging a ", "praying to a ", "riding a ", "making a ", "going through a portal with a ", "invading America for a ", "forgot everything because of a " ],
-    [ "car.", "cat.", "couch.", "door.", "television.", "computer case.", "carpet", "grenade.", "soldier.", "lettuce." ]
-],
-generatedExcuse = [];
+    ["I was ", "My neighbor was ", "We were ", "My mom was ", "My sister was ", "My brother was ", "My girlfriend was ", "My boyfriend was ", "My uncle was "],
+    ["washing my ", "eating a ", "damaging a ", "praying to a ", "riding a ", "making a ", "going through a portal with a ", "invading America for a ", "forgot everything because of a ", "giving birth to "],
+    ["car.", "cat.", "couch.", "door.", "television.", "computer case.", "carpet", "grenade.", "soldier.", "lettuce.", " my cousin."]
+];
 
 // A funny list, you can create a pull request to add something :]
 
-for (choiceFirst of excuses) 
-    generatedExcuse.push(choiceFirst[Math.floor(Math.random() * choiceFirst.length)]);
-
-console.log(generatedExcuse.join(""));
+console.log(excuses.map(choice => choice[Math.floor(Math.random() * choice.length)]).join(""));
